a124e9f66050aa47f4b6508b3701d4ac8fcc9fd3
alex <lesha.sinitsin@mail.ru>
2022-07-03 18:30:18 +0300

    test eslint prettier huski

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
-import { DownloadOutlined } from '@ant-design/icons';
-import { Button, Radio, Tooltip } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-import { SizeType } from 'antd/lib/config-provider/SizeContext';
+import { DownloadOutlined } from '@ant-design/icons'
+import { SearchOutlined } from '@ant-design/icons'
+import { Button, Radio, Tooltip } from 'antd'
+import { SizeType } from 'antd/lib/config-provider/SizeContext'
 import { useState } from 'react'
 import './App.less'
 const App = () => {
-  const [size, setSize] = useState<SizeType>('large');
+  const [size, setSize] = useState<SizeType>('large')
   return (
     <>
       <Radio.Group value={size} onChange={(e) => setSize(e.target.value)}>
-        <Radio.Button value="large">Large</Radio.Button>
-        <Radio.Button value="default">Default</Radio.Button>
-        <Radio.Button value="small">Small</Radio.Button>
+        <Radio.Button value='large'>Large</Radio.Button>
+        <Radio.Button value='default'>Default</Radio.Button>
+        <Radio.Button value='small'>Small</Radio.Button>
       </Radio.Group>
       <br />
       <br />
-      <Button type="primary" size={size}>
+      <Button type='primary' size={size}>
         Primary
       </Button>
       <Button size={size}>Default</Button>
-      <Button type="dashed" size={size}>
+      <Button type='dashed' size={size}>
         Dashed
       </Button>
       <br />
-      <Button type="link" size={size}>
+      <Button type='link' size={size}>
         Link
       </Button>
-      <Tooltip title="search">
-      <Button shape="circle" icon={<SearchOutlined />} size="large" />
-    </Tooltip>
+      <Tooltip title='search'>
+        <Button shape='circle' icon={<SearchOutlined />} size='large' />
+      </Tooltip>
       <br />
-      <Button type="primary" icon={<DownloadOutlined />} size={size} />
-      <Button type="primary" shape="circle" icon={<DownloadOutlined />} size={size} />
-      <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size} />
-      <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size}>
+      <Button type='primary' icon={<DownloadOutlined />} size={size} />
+      <Button type='primary' shape='circle' icon={<DownloadOutlined />} size={size} />
+      <Button type='primary' shape='round' icon={<DownloadOutlined />} size={size} />
+      <Button type='primary' shape='round' icon={<DownloadOutlined />} size={size}>
         Download
       </Button>
-      <Button type="primary" icon={<DownloadOutlined />} size={size}>
+      <Button type='primary' icon={<DownloadOutlined />} size={size}>
         Download
       </Button>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
